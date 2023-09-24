408a4c6643ed00d3faad5b5eb5d452dd7f5e070c
M-U-16 <132483300+M-U-16@users.noreply.github.com>
2023-09-24 12:51:29 +0200

    sqlite database

--- flask_app/db.py
+++ flask_app/db.py
@@ -24,3 +24,41 @@ def printTable():
     cursor = con.cursor()
     table = cursor.execute("SELECT * FROM wetterdaten").fetchall()
     print(table)
+    
+class Database:
+    def __init__(self):
+        self.con = 0
+        self.path = ""
+    
+    def __init__(self, path):
+        self.setPath(path)
+        self.setConnection()
+        self.setCursor()
+    
+    #set path for database
+    def setPath(self, path):
+        self.path = path
+    #set connection
+    def setConnection(self)
+        try:
+            self.con = sqlite3.connect(self.path)
+        except Error as e:
+            print(e)
+    #set cursor
+    def setCursor(self):
+        self.cursor = self.con.cursor()
+    
+    def queryDatabase(self, query):
+        cursor = self.con.cursor()
+        return cursor.execute(query)
+
+    def addData(self, table_name, data):
+        date = data["time"]
+        temp = data["temp"]
+        humi = data["humi"]
+        pres = data["pres"]
+        lux = data["lux"]
+        self.cursor.execute("INSERT INTO ? VALUES (?, ?, ?, ?, ?)", (table_name ,date, temp, humi, pres, lux))
+        self.con.commit()
+        
+        
---
+++ flask_app/server_settings.py
@@ -0,0 +1,9 @@
+SERVER_SETTINGS = {
+    "ip/host": "localhost",
+    "port": 80,
+    "database": "wetter.db",
+    "wetter_table": "wetterdaten"
+}
+PI_SETTINGS = {
+    "rpm": 100
+}
