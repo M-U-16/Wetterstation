bb9942a2dc95253ee3a16becf6dcde4f6f6c6e51
M-U-16 <132483300+M-U-16@users.noreply.github.com>
2023-09-25 22:08:05 +0200

    testing around

--- flask_app/static/js/mainOffset.js
+++ flask_app/static/js/mainOffset.js
@@ -1,6 +1,5 @@
 //script for setting a offset to the main element
-window.onload = () => {
-    const main = document.querySelector("#main")
-    const navbarHeight = document.querySelector("#app__navbar").getBoundingClientRect().height
-    main.style.marginTop = `${navbarHeight}px`
-}
+/* const main = document.querySelector("#main") */
+const navbarHeight = document.querySelector("#app__navbar").getBoundingClientRect().height
+/* main.style.marginTop = `${navbarHeight}px` */
+document.body.style.setProperty("--navbar-height", `${navbarHeight}px`)
