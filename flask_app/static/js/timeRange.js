4423c3bdb46c0ad5bbc55216588d6504a6a7b36e
M-U-16 <132483300+M-U-16@users.noreply.github.com>
2023-11-10 22:00:58 +0100

    selecting time range feature

--- flask_app/static/js/timeRange.js
+++ flask_app/static/js/timeRange.js
@@ -1,34 +1,15 @@
-const timeRangeBtn = document.getElementById("time-select-button")
-const selectOptions = document.querySelectorAll(".app__select-option")
-const selectContainer = document.querySelector(".app__select-options-container")
-const indicatorArrow = document.querySelector("#select-arrow-icon")
-const timeRangeDisplay = document.querySelector("#time-select-text")
-let selectedState = false
-let currentFrequency
-const activeContainerClass = "active-select-container"
+const form = document.getElementById("range-form-element")
+form.addEventListener("submit", (e) => { postRangeForm(e) })
 
-timeRangeBtn.dataset.current = "1d"
-
-const closeContainer = () => {
-    selectContainer.classList.remove(activeContainerClass)
-    indicatorArrow.style.rotate = "0deg"
-    selectedState = false
-}
-
-timeRangeBtn.addEventListener("click", () => {
-    selectContainer.classList.toggle(activeContainerClass)
-    selectedState = !selectedState
-    if (!selectedState) indicatorArrow.style.rotate = "0deg"
-    if (selectedState) indicatorArrow.style.rotate = "180deg"
-})
-
-selectOptions.forEach(option => {
-    option.addEventListener("click", () => {
-        if (option.innerHTML != timeRangeDisplay.innerHTML) {
-            timeRangeDisplay.innerHTML = option.innerHTML
-            timeRangeBtn.dataset.current = option.dataset.value
-            currentFrequency = option.dataset.value
-            closeContainer()
+const postRangeForm = (e) => {
+    e.preventDefault()
+    const childs = e.target.children
+    const dates = {}
+    for (const child in childs) {
+        const elm = childs[child]
+        if (elm.nodeName === "INPUT") {
+            dates[elm.name] = elm.value
         }
-    })
-})
+    }
+    console.log(dates)
+}
