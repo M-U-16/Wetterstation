6023a95df573d81849c4a35c923621187508c940
M-U-16 <132483300+M-U-16@users.noreply.github.com>
2023-11-15 23:10:11 +0100

    testing with threading

---
+++ Wettereinheit/test.py
@@ -0,0 +1,23 @@
+from time import *
+import threading
+from datetime import datetime
+running = True
+
+def test():
+    thread_start = datetime.now().strftime("%H:%M:%S")
+    while running:
+        #print("GETTING DATA...")
+        sleep(1000)
+    thread_end = datetime.now().strftime("%H:%M:%S")
+    
+    print("Programm Start: " + thread_start)
+    print("Programm Ende: " + thread_end)
+    
+bg_thread = threading.Thread(target=test)
+bg_thread.start()
+
+print("---RUNNING NOW!!!---")
+while running:
+    user_input = input("Type 'STOP' to stop the programm: ")
+    if user_input == "STOP":
+        running = False
