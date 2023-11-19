a8b8af4deef10496e5764b07499ba2dcf1c635e4
M-U-16 <132483300+M-U-16@users.noreply.github.com>
2023-11-19 19:36:51 +0100

    fixing stuff and refactoring

--- flask_app/server.py
+++ flask_app/server.py
@@ -1,26 +1,10 @@
-# -*- coding: utf-8 -*-
-# Git repo <https://gitlab.com/idotj/enviroplusweb>
-# Forked from <https://github.com/nophead/EnviroPlusWeb>
-# EnviroPlusWeb is free software: you can redistribute it and/or modify it under the terms of the
-# GNU General Public License as published by the Free Software Foundation, either version 3 of
-# the License, or (at your option) any later version.
-# EnviroPlusWeb is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
-#
-import math
-import sqlite3
-from flask import Flask, g, render_template, url_for, request, session
-from livereload import Server
 import logging
+from flask import Flask, render_template
 from server_settings import SERVER_SETTINGS
 
-from db import *
-
-#import routes
-from routes.app.wetter import wetter_route
+#importing routes
 from routes.app.einstellungen import einstellungen_route
-
+from routes.app.wetter import wetter_route
 from routes.api_router import api_route
 
 app = Flask(__name__)
@@ -28,7 +12,6 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.secret_key = 'BAD_SECRET_KEY'
 log = logging.getLogger("werkzeug")
 log.disabled = True
-run_flag = True
 
 gas_sensor = None
 particulate_sensor = None
@@ -52,6 +35,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    
-    app.run(debug = True, host = 'localhost', port = 80, use_reloader = False)
-    run_flag = False
+    app.run(debug=True, host='localhost', port=80, use_reloader=True)
