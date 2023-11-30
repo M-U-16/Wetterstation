ca19e100ac2350f0c906137deb172acd2164f2a7
M-U-16 <132483300+M-U-16@users.noreply.github.com>
2023-12-01 00:38:28 +0100

    menu fixing

--- Server/static/js/components/menu.js
+++ Server/static/js/components/menu.js
@@ -6,24 +6,19 @@ console.log(menuHamburger)
 // Toggle menu by icon click
 menuMainBtn.addEventListener("click", function () {
   menuHamburger.classList.toggle("active-hamburger");
-  menuMainContainer.classList.toggle("app-menu-open");
+  menuMainContainer.classList.toggle("app-open-menu");
   // Detect outside click
-  document.addEventListener("click", function clickOutsideMenu(event) {
-    let clickMenuContainer = menuMainContainer.contains(event.target);
-    let clickMenuBtn = menuMainBtn.contains(event.target);
-    if (
-      !clickMenuContainer &&
-      !clickMenuBtn &&
-      menuMainContainer.classList.contains("app-menu-open")
-    ) {
-      // Close menu
-      menuMainBtn.classList.toggle("btn-active");
-      menuMainBtn.setAttribute(
-        "aria-expanded",
-        menuMainBtn.classList.contains("btn-active")
-      );
-      menuMainContainer.classList.toggle("menu-settings-open");
-      document.removeEventListener("click", clickOutsideMenu);
-    }
-  });
+});
+document.addEventListener("click", function clickOutsideMenu(event) {
+  let clickMenuContainer = menuMainContainer.contains(event.target);
+  let clickMenuBtn = menuMainBtn.contains(event.target);
+  if (
+    !clickMenuContainer &&
+    !clickMenuBtn &&
+    menuMainContainer.classList.contains("app-open-menu")
+  ) {
+    // Close menu
+    menuMainContainer.classList.toggle("app-open-menu");
+    document.removeEventListener("click", clickOutsideMenu);
+  }
 });
