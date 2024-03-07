599eb0773cc845159c08311f5ad06815146ba7ca
M-U-16 <132483300+M-U-16@users.noreply.github.com>
2024-03-07 22:59:34 +0100

    working on sensors

--- Wettereinheit/sensors/bme.py
+++ Wettereinheit/sensors/bme.py
@@ -1,21 +1,57 @@
-import os
 import time
 import smbus2
 import bme280
+from datetime import datetime
 
 class BME:
     def __init__(self, address, timeout):
         self.bus = smbus2.SMBus(1)
         self.address = address
         self.timeout = timeout
+        self.running = False
         self.calibration = bme280.load_calibration_params(
             self.bus,
             self.address
         )
-        
-    def read(self):
-        data = bme280.sample(self.bus, self.calibration)
-        print(data)
-        
-    def run(self, queue):
-        pass
+
+    # reads the current temperature, humidity and
+    # pressure and if set to true(default) also adds current date
+    # -----------------------------------------------------------
+    def read(self, date=True):
+        sample = bme280.sample(
+            self.bus,
+            self.address,
+            self.calibration
+        )
+        data = {
+            "temp": sample.temperature,
+            "humi": sample.humidity,
+            "pressure": sample.pressure
+        }
+        if date: data["date"] = datetime.now()
+        return data
+
+    # handler for messages from pipe
+    # in start method
+    # -> expects message from main process
+    # ------------------------------------
+    def handleMsg(self, msg):
+        if msg == "stop-running":
+            self.running = False
+
+    # continuously reads bme sensor data
+    # and puts it on a queue for the main process
+    # -> should be started in external process
+    # -------------------------------------------
+    def start(self, msgPipe, dataQueue):
+        self.running = True
+
+        while self.running:
+            #check if process should end
+            if msgPipe.poll():
+                self.handleMsg(msgPipe.recv())
+
+            data = self.read(True)
+            dataQueue.put(data)
+            time.sleep(self.timeout)
+        msgPipe.send("process-stopped")
---
+++ Wettereinheit/sensors/sound.py
@@ -0,0 +1,50 @@
+import sys
+import time
+import argparse
+import soundfile as sf
+import sounddevice as sd
+from multiprocessing import Queue, Process, Pipe
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("-lsd", "--list-devices", action="store_true")
+args = parser.parse_args()
+if args.list_devices:
+    print(sd.query_devices())
+    sys.exit(0)
+
+def Record(options, pipe):
+    queue = Queue()
+    
+    def callback(data, frames, time, status):
+        queue.put(data.copy())
+
+    soundfile = sf.SoundFile(
+        options["filename"], mode="w",
+        samplerate=options["samplerate"],
+        channels=options["channels"],
+        subtype=options["subtype"]
+    )
+    stream = sd.InputStream(
+        callback=callback,
+        channels=options["channels"],
+        device=options["device"]
+    )
+    
+    end = round(time.time() + options["duration"])
+    print(end, file=sys.stdout)
+    with soundfile as file:
+        with stream:
+            while time.time() < end:
+                file.write(queue.get())
+
+""" options = {
+    "filename": "test.wav",
+    "samplerate": 44100,
+    "subtype": "PCM_16",
+    "channels": 2,
+    "device": 1,
+    "duration": 5
+} """
+
+recordProcPipe, recordMainPipe = Pipe(duplex=True)
+recordProc = Process(target=Record, args=(options, recordProcPipe,))
