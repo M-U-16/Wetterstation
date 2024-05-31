01e6cb60f023e44031df4fd4b8ddcf78115dc920
M-U-16 <132483300+M-U-16@users.noreply.github.com>
2024-05-31 07:17:18 +0200

    b

--- Server/run.py
+++ Server/run.py
@@ -1,7 +1,7 @@
 # config import
 from settings import load_config
 load_config(".env.dev")
-
+# -------------
 from app import create_app
 app = create_app()
 
--- Server/wsgi.py
+++ Server/wsgi.py
@@ -1,6 +1,6 @@
-from app import create_app
 # config import
 from settings import load_config
 load_config(".env.dev")
 # -------------
+from app import create_app
 app = create_app()
